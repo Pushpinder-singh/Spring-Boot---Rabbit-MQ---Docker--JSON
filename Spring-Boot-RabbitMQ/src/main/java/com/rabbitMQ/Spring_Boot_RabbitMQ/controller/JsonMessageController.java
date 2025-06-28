1822a1a88f3b4309a16222bfe07be21f1dd5bf40
pushpinder singh <pushpinder.1990singh@gmail.com>
2025-06-28 11:42:26 -0400

    Rest API controller and DTO

---
+++ Spring-Boot-RabbitMQ/src/main/java/com/rabbitMQ/Spring_Boot_RabbitMQ/controller/JsonMessageController.java
@@ -0,0 +1,10 @@
+package com.rabbitMQ.Spring_Boot_RabbitMQ.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/API/v1")
+public class JsonMessageController {
+
+}
---
+++ Spring-Boot-RabbitMQ/src/main/java/com/rabbitMQ/Spring_Boot_RabbitMQ/dto/Employee.java
@@ -0,0 +1,10 @@
+package com.rabbitMQ.Spring_Boot_RabbitMQ.dto;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	private int id;
+    private String firstName;
+    private String lastName;
+}
