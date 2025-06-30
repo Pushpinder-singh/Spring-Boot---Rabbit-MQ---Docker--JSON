1b6d742984d6a0709cd763c05163fc496db44ae3
pushpinder singh <pushpinder.1990singh@gmail.com>
2025-06-29 20:30:54 -0400

    JSON Message Producer and REst API

--- Spring-Boot-RabbitMQ/src/main/java/com/rabbitMQ/Spring_Boot_RabbitMQ/controller/JsonMessageController.java
+++ Spring-Boot-RabbitMQ/src/main/java/com/rabbitMQ/Spring_Boot_RabbitMQ/controller/JsonMessageController.java
@@ -1,10 +1,36 @@
 package com.rabbitMQ.Spring_Boot_RabbitMQ.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rabbitMQ.Spring_Boot_RabbitMQ.dto.Employee;
+import com.rabbitMQ.Spring_Boot_RabbitMQ.producer.JsonMessageProducer;
+
 @RestController
-@RequestMapping("/API/v1")
+@RequestMapping("/api/v1")
 public class JsonMessageController {
 
+	@Autowired
+	private JsonMessageProducer jsonMessageProducer; 
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(JsonMessageController.class);
+	
+	@GetMapping("up")
+	public ResponseEntity<String> up(){
+		return ResponseEntity.ok("Server is up !!");
+	}
+	
+	@PostMapping("/publish")
+	public ResponseEntity<String> sendJSONMessage(@RequestBody Employee emp) {
+		jsonMessageProducer.sendJsonMessage(emp);
+		LOGGER.info(String.format("Json Payload sent -> %s", emp.toString()));
+		return ResponseEntity.ok("Message has been produced !!");
+	}
 }
--- Spring-Boot-RabbitMQ/src/main/java/com/rabbitMQ/Spring_Boot_RabbitMQ/producer/JsonMessageProducer.java
+++ Spring-Boot-RabbitMQ/src/main/java/com/rabbitMQ/Spring_Boot_RabbitMQ/producer/JsonMessageProducer.java
@@ -1,5 +1,37 @@
 package com.rabbitMQ.Spring_Boot_RabbitMQ.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.rabbitMQ.Spring_Boot_RabbitMQ.dto.Employee;
+
+@Service
 public class JsonMessageProducer {
 
+	/* Message producer firstly fetch the values of Exhange and routing key from properties file
+	 *  and send a message to queue by using sendAndConvert()
+	 * */
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(JsonMessageProducer.class);
+	
+	@Value("${rabbitmq.exchange.name}")
+	private String exchange;
+	
+	@Value("{rabbitmq.routing.json.key}")
+	private String Routing_Key;
+	
+	// Dependency Injection
+	@Autowired
+	private RabbitTemplate rabbitTemplate; 
+	
+	
+	public void sendJsonMessage(Employee emp){
+        LOGGER.info(String.format("Json Payload sent -> %s", emp.toString()));
+        //rabbitTemplate.convertAndSend(exchange, Routing_Key, emp);
+    }
+	
 }
